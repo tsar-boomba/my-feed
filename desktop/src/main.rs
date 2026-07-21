aefb5fb92bf664e29d7a56deefbc85b327beff13
tsar-boomba <itg.2048@gmail.com>
2026-07-20 22:01:39 -0700

    use card component, add webview

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -8,18 +8,23 @@ mod http;
 mod models;
 mod queries;
 
-use std::time::Duration;
+use std::{rc::Rc, time::Duration};
 
 use freya::{
     prelude::*,
     query::{Query, use_query},
     router::*,
+    webview::{WebView, WebViewPlugin},
 };
 
 use crate::{models::GetItemsReturn, queries::GetItems};
 
 fn main() {
-    launch(LaunchConfig::new().with_window(WindowConfig::new(app)))
+    launch(
+        LaunchConfig::new()
+            .with_plugin(WebViewPlugin::new())
+            .with_window(WindowConfig::new(app)),
+    )
 }
 
 fn app() -> impl IntoElement {
@@ -44,7 +49,23 @@ impl Component for Layout {
 struct Home {}
 impl Component for Home {
     fn render(&self) -> impl IntoElement {
+        let mut post_url = use_state(|| None::<Rc<str>>);
         let res = use_query(Query::new((), GetItems).interval_time(Duration::from_mins(1)));
+
+        if let Some(post_url_str) = post_url.read().as_deref() {
+            return rect()
+                .direction(Direction::Vertical)
+                .child(
+                    Button::new()
+                        .filled()
+                        .padding(8.)
+                        .cursor_icon(CursorIcon::Pointer)
+                        .on_press(move |_| *post_url.write() = None)
+                        .child("Back"),
+                )
+                .child(WebView::new(post_url_str).expanded())
+                .into_element();
+        }
 
         let content = match &*res.read().state() {
             freya::query::QueryStateData::Pending => "ready!".into_element(),
@@ -57,47 +78,57 @@ impl Component for Home {
                 .main_align(Alignment::Center)
                 .padding(8.)
                 .spacing(8.)
-                .children(
-                    res.as_ref()
-                        .unwrap()
-                        .iter()
-                        .map(|item| Card { item: item.clone() }.into_element()),
-                )
+                .children(res.as_ref().unwrap().iter().map(|item| {
+                    Card {
+                        item: item.clone(),
+                        post_url: post_url.clone(),
+                    }
+                    .into_element()
+                }))
                 .into_element(),
         };
 
         ScrollView::new()
             .direction(Direction::Vertical)
             .child(content)
+            .into_element()
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 struct Card {
     item: GetItemsReturn,
+    post_url: State<Option<Rc<str>>>,
 }
 
 impl Component for Card {
     fn render(&self) -> impl IntoElement {
-        rect()
-            .direction(Direction::Vertical)
-            .padding(4.)
-            .corner_radius(4.)
-            .border(Border::new().width(1.).fill((127, 127, 127)))
+        let mut post_url = self.post_url.clone();
+        let link: Rc<str> = Rc::from(&*self.item.item.link);
+
+        freya::components::Card::new()
             .child(
-                Link::new(&*self.item.item.link).child(
-                    label()
-                        .theme_color()
-                        .font_weight(FontWeight::BOLD)
-                        .font_size(24.)
-                        .text(
-                            self.item
-                                .item
-                                .title
-                                .clone()
-                                .unwrap_or(self.item.item.link.clone()),
-                        ),
-                ),
+                label()
+                    .theme_color()
+                    .font_weight(FontWeight::BOLD)
+                    .font_size(24.)
+                    .on_press(move |_| {
+                        Cursor::set(CursorIcon::default());
+                        *post_url.write() = Some(link.clone());
+                    })
+                    .on_pointer_enter(move |_| {
+                        Cursor::set(CursorIcon::Pointer);
+                    })
+                    .on_pointer_leave(move |_| {
+                        Cursor::set(CursorIcon::default());
+                    })
+                    .text(
+                        self.item
+                            .item
+                            .title
+                            .clone()
+                            .unwrap_or(self.item.item.link.clone()),
+                    ),
             )
     }
 
